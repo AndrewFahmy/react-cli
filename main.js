357e8e6887f02dc6c00f25111b6bd186755ecdb3
Andrew Fahmy <fahmyand@utccgl.com>
2020-05-31 23:28:43 +0200

    updating app version

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ arguments = serveCommand.bindServeCommand(arguments);
 arguments = buildCommand.bindBuildCommand(arguments);
 
 arguments.scriptName('')
-    .version('1.0.3')
+    .version('1.0.4')
     .help()
     .alias('h', 'help')
     .alias('v', 'version').argv;
